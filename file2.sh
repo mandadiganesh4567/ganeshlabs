24e74bcd343f04b26670620595d6d3805ff802d8
Ganesh Mandadi <mandadiganesh4567@gmail.com>
2025-09-28 13:10:40 +0530

    shell script

--- file2.sh
+++ file2.sh
@@ -2,13 +2,16 @@
 var1="$1"
 var2="$2"
 date=$(date)
-read -p " provide if you havent pass arguments "
+if [ -z "$var1" ] || [  -z "$var2" ]; then
+    read -p " give name 1:  var1 "
+    read -p " give name 2:  var2 "
+if
 echo "new learner is : $var1"
 echo "$var1  saying Thanks alot !!!! to $var2 at $date"
 
 
 DISK_USAGE="85"
-if [ $DISK_USAGE -gt 90 ]; then
+if [ "$DISK_USAGE" -gt 90 ]; then
     echo "Critical alert! Disk space running low."
 else
     echo "Disk fine"
