bf6d388fff7737e6cde6729157756bc3a4b774d4
Ganesh Mandadi <mandadiganesh4567@gmail.com>
2025-09-28 12:47:55 +0530

    shell script

--- file2.sh
+++ file2.sh
@@ -4,3 +4,11 @@ var2="Gemini Ai"
 date=$(date)
 echo "new learner is : $var1+$var2"
 echo "$var1  saying Thanks alot !!!! to $var2 at $date"
+
+DISK_USAGE="85"
+if [$DISK_USAGE -gt 90]
+then
+echo "Critical alert! Disk space running low."
+else
+echo "Disk fine"
+fi
