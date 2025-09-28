8d73da390fe2a570b1cdb6b57775b2975a4c6679
Ganesh Mandadi <mandadiganesh4567@gmail.com>
2025-09-28 13:03:44 +0530

    shell script

--- file2.sh
+++ file2.sh
@@ -2,9 +2,10 @@
 var1="$1"
 var2="$2"
 date=$(date)
-echo "new learner is : $var1+$var2"
+read -p " provide if you havent pass arguments "
+echo "new learner is : $var1"
 echo "$var1  saying Thanks alot !!!! to $var2 at $date"
-read -p " provide if you havent pass arguments $@"
+
 
 DISK_USAGE="85"
 if [ $DISK_USAGE -gt 90 ]; then
