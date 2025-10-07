dab64ab5a1fb06d319093dda1f3bb223b19a89e7
Ganesh Mandadi <mandadiganesh4567@gmail.com>
2025-10-08 00:49:41 +0530

    LEarding fucntions

--- Redirectiors.sh
+++ Redirectiors.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-LOGS_FOLD="/var/logs/script"
+LOGS_FOLDER="/var/logs/script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1) 
 TIMESTAMP=$(date +%Y-%m-%D-%H-%M-%S)
-LOG_FIL="$LOGS_FOLD/$SCRIPT_NAME-$TIMESTAMP.log"
+LOG_FILDER="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 
-mkdir -p $LOGS_FOLD
+mkdir -p $LOGS_FOLDER
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
@@ -14,16 +14,16 @@ VALIDATE(){
 
   if [ $? -ne 0 ]
   then
-    echo -e " $1 is $R failed $N" &>>$LOG_FIL
+    echo -e " $1 is $R failed $N" &>>$LOG_FILDER
     echo " $3 reason: " &>>$LOG_FIL
   else  
-    echo -e "$2 is $G success $N" &>>$LOG_FIL
+    echo -e "$2 is $G success $N" &>>$LOG_FILDER
   fi
 }
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-  echo "its not in privillage mode run with privillage mode" &>>$LOG_FIL
+  echo "its not in privillage mode run with privillage mode" &>>$LOG_FILDER
   exit 1
 fi
 
@@ -34,10 +34,10 @@ do
 
     if [ $? -ne 0 ]
     then
-      echo -e " $R installation faileed and sql not present$N" &>>$LOG_FIL
+      echo -e " $R installation faileed and sql not present$N" &>>$LOG_FILDER
       dnf install $package -y
-      VALIDATE $? "Listing $package" "may be dnf failed" &>>$LOG_FIL
+      VALIDATE $? "Listing $package" "may be dnf failed" &>>$LOG_FILDER
     else  
-      echo -e "$G $package alreday installed enjoy$N" &>>$LOG_FIL
+      echo -e "$G $package alreday installed enjoy$N" &>>$LOG_FILDER
     fi
 done
