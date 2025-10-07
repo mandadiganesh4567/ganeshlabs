d5d07dd0644a8680fa7d407e6494743a90b04ba8
Ganesh Mandadi <mandadiganesh4567@gmail.com>
2025-10-07 20:45:45 +0530

    Learning functions

--- file3.sh
+++ file3.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+Validate(){
+
+  if [ $? -ne 0 ]
+  then
+    echo " $1 is failed"
+    echo " $3 reason: "
+  else  
+    echo " $2 is success"
+}
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
@@ -8,12 +17,13 @@ then
 fi
 
 dnf list installed mysql -y
-
-
-if [ $? -ne 0 ]
-then
-  echo " installation faileed and sql not present"
-  dnf install mysql -y
-else  
-  echo " alreday installed enjoy"
-fi
+VALIDATE $? "Listing Mysql" "may be dnf failed"
+dnf list installed git -y
+VALIDATE $? "Listing git" "may be dnf failed"
+#if [ $? -ne 0 ]
+# then
+#   echo " installation faileed and sql not present"
+#   dnf install mysql -y
+# else  
+#   echo " alreday installed enjoy"
+# fi
