13cd10e4abe443b9406db9328c3a9a7d47744af4
Ganesh Mandadi <mandadiganesh4567@gmail.com>
2025-10-09 12:24:34 +0530

    Learning Redirectors

--- file3.sh
+++ file3.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+LOging="/var/logs/script"
+Timestamp=$(date +%H%M%S)
+Name=$(echo $0 | cut d "'." f1)
+LogFilee=$LOging\ $Name-$Timestamp.log
 
+mkdir -p LOging
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
@@ -9,16 +14,16 @@ VALIDATE(){
 
   if [ $? -ne 0 ]
   then
-    echo -e " $1 is $R failed $N"
+    echo -e " $1 is $R failed $N" &>>LogFilee
     echo " $3 reason: "
   else  
-    echo -e "$2 is $G success $N"
+    echo -e "$2 is $G success $N" &>>LogFilee
   fi
 }
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-  echo "its not in privillage mode run with privillage mode"
+  echo "its not in privillage mode run with privillage mode" &>>LogFilee
   exit 1
 fi
 
@@ -29,21 +34,21 @@ do
 
     if [ $? -ne 0 ]
     then
-      echo -e " $R installation faileed and sql not present$N"
+      echo -e " $R installation faileed and sql not present$N" &>>LogFilee
       dnf install $package -y
-      VALIDATE $? "Listing $package" "may be dnf failed"
+      VALIDATE $? "Listing $package" "may be dnf failed" &>>LogFilee
     else  
-      echo -e "$G $package alreday installed enjoy$N"
+      echo -e "$G $package alreday installed enjoy$N" &>>LogFilee
     fi
 done
 
-# dnf list installed git -y
+dnf list installed git -y
 
-# if [ $? -ne 0 ]
-# then
-#   echo -e " $R installation faileed and sql not present$N"
-#   dnf install git -y
-#   VALIDATE $? "Listing git" "may be dnf failed"
-# else  
-#   echo -e " $G alreday installed enjoy$N"
-# fi
+if [ $? -ne 0 ]
+then
+  echo -e " $R installation faileed and sql not present$N" &>>LogFilee
+  dnf install git -y
+  VALIDATE $? "Listing git" "may be dnf failed" &>>LogFilee
+else  
+  echo -e " $G alreday installed enjoy$N" &>>LogFilee
+fi
