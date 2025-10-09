af0181f0549d0de091e5a241a309dfb586a650fe
Ganesh Mandadi <mandadiganesh4567@gmail.com>
2025-10-09 12:47:09 +0530

    Learning Redirectors

--- file3.sh
+++ file3.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-LOging="/var/logs/script"
-Timestamp=$(date +%H%M%S)
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 
-# Extract the script name without the extension using 'cut'
-# $0 is the name of the script. We pipe it to 'cut' to use the dot '.' as a delimiter
-# and take the first field (-f1).
-Name=$(echo "$0" | cut -d "." -f1 ) # A more complete cut 
-
-# Full path to the log file (Note the use of '/' for a path separator)
-LogFile="$LOging/$Name-$Timestamp.log"
-
-# --- Directory Creation ---
-# Create the log directory if it doesn't exist (Note the '$' for variable reference)
-mkdir -p "$LOging" 
+mkdir -p $LOGS_FOLDER
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
@@ -22,16 +14,16 @@ VALIDATE(){
 
   if [ $? -ne 0 ]
   then
-    echo -e " $1 is $R failed $N" &>>LogFilee
+    echo -e " $1 is $R failed $N" &>>LOG_FILE
     echo " $3 reason: "
   else  
-    echo -e "$2 is $G success $N" &>>LogFilee
+    echo -e "$2 is $G success $N" &>>LOG_FILE
   fi
 }
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-  echo "its not in privillage mode run with privillage mode" &>>LogFilee
+  echo "its not in privillage mode run with privillage mode" &>>LOG_FILE
   exit 1
 fi
 
@@ -42,11 +34,11 @@ do
 
     if [ $? -ne 0 ]
     then
-      echo -e " $R installation faileed and sql not present$N" &>>LogFilee
+      echo -e " $R installation faileed and sql not present$N" &>>LOG_FILE
       dnf install $package -y
-      VALIDATE $? "Listing $package" "may be dnf failed" &>>LogFilee
+      VALIDATE $? "Listing $package" "may be dnf failed" &>>LOG_FILE
     else  
-      echo -e "$G $package alreday installed enjoy$N" &>>LogFilee
+      echo -e "$G $package alreday installed enjoy$N" &>>LOG_FILE
     fi
 done
 
@@ -54,9 +46,9 @@ dnf list installed git -y
 
 if [ $? -ne 0 ]
 then
-  echo -e " $R installation faileed and sql not present$N" &>>LogFilee
+  echo -e " $R installation faileed and sql not present$N" &>>LOG_FILE
   dnf install git -y
-  VALIDATE $? "Listing git" "may be dnf failed" &>>LogFilee
+  VALIDATE $? "Listing git" "may be dnf failed" &>>LOG_FILE
 else  
-  echo -e " $G alreday installed enjoy$N" &>>LogFilee
+  echo -e " $G alreday installed enjoy$N" &>>LOG_FILE
 fi
